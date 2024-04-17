f7bf311502218d643064cb2b9e732fd929460c33
Jayvir Singh <sjayvir09@gmail.com>
2024-04-18 01:32:12 +0530

    Day-1 changes

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1>My first React App</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  
+
+  return (
+    <h1>This is first vite app</h1>
+  )
+}
+
+export default App

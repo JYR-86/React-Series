831c0a003b1b017b87a4255fb49c41beae00db5d
Jayvir Singh <sjayvir09@gmail.com>
2024-06-23 00:50:33 +0530

    updates

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,33 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind Injection</h1>
+      <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+  <img class="w-24 h-24 rounded-full mx-auto" src="https://images.pexels.com/photos/39853/woman-girl-freedom-happy-39853.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" width="384" height="512" />
+  <div class="pt-6 space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption class="font-medium">
+      <div cla>
+        Sarah Dayan
+      </div>
+      <div>
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+      </figure>
+      
+    </>
+  )
+}
+
+export default App
